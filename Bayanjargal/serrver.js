f78e76d1767e8aae35a0ee96722d017d3c7b24d8
mstars_lab3_15 <mstars_lab3_15@mstars-lab3-15s-iMac.local>
2022-06-23 09:21:38 +0800

    [added] text

--- Bayanjargal/serrver.js
+++ Bayanjargal/serrver.js
@@ -2,7 +2,7 @@ var http = require("http");
 http
   .createServer(function (request, response) {
     response.writeHead(200);
-    response.write("<h1>Hello Node !!!!</h1>\n");
+    response.write("<h1>Hello Node 6/23!!!!</h1>\n");
     response.end();
   })
   .listen(3000);
