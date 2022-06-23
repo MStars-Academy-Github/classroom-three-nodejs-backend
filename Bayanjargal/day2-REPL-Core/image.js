a42f7d566003a2815c75d65df0eadebcdf63e93f
mstars_lab3_15 <mstars_lab3_15@mstars-lab3-15s-iMac.local>
2022-06-23 13:52:03 +0800

    [added] delete console.log

--- Bayanjargal/day2-REPL-Core/image.js
+++ Bayanjargal/day2-REPL-Core/image.js
@@ -1,9 +1,8 @@
 const http = require("http");
 const fs = require("fs");
 const image = `${__dirname}/data/image/download.png`;
-const audio = `${__dirname}/data/audio/audio.mp3`;
-const video = `${__dirname}/data/video/video.mp4`;
-console.log(filename);
+
+
 
 http
   .createServer(function (request, response) {
@@ -14,8 +13,3 @@ http
   })
   .listen(3000);
 console.log("server running at http://localhost:3000");
-const bayna = {
-  name: "bayna",
-  age: "24",
-  intrest: "basketball",
-};
