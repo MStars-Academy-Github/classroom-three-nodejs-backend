b666c5be7b16e784d386f311c0e8a490f4a3fc65
mstars_lab3_15 <mstars_lab3_15@mstars-lab3-15s-iMac.local>
2022-06-23 12:34:06 +0800

    [added] audio,video,json data

---
+++ Bayanjargal/day2-REPL-Core/serveraudio.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const fs = require("fs");
+const audio = `${__dirname}/data/audio/audio.mp3`;
+http
+  .createServer(function (request, response) {
+    response.setHeader("Content-Type", "audio/mpeg");
+    fs.createReadStream(audio)
+      .on("error", () => console.log("err"))
+      .pipe(response);
+  })
+  .listen(3000);
+console.log("server running at http://localhost:3000");
+const bayna = {
+  name: "bayna",
+  age: "24",
+  intrest: "basketball",
+};
