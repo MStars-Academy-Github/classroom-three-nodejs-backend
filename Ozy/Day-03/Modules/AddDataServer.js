6c02e7d2c8ed4884ca3d017ebce6305fcf9dd6d9
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-06-24 13:46:47 +0800

    POST request

---
+++ Ozy/Day-03/Modules/AddDataServer.js
@@ -0,0 +1,45 @@
+const http = require("http")
+const fs = require("fs")
+
+
+http.createServer((request, response)=>{
+    console.log(`Request URL is: ${request.url}`);
+    console.log(`Request Method is : ${request.method}`);
+    
+
+    if(request.url === "/add/food"){
+        console.log("add food");
+        if(request.method === "POST"){
+            console.log("it is add food Post method");
+            request.on("data", chunk=>{
+                console.log(`Data chunk avaible : ${chunk}`);
+                fs.readFile("./data/foods.json", "utf-8", (err, data)=>{
+                    if(err){
+                        console.error(err)
+                        return
+                    }else{
+                        let arr = JSON.parse(data)
+                        let newData = JSON.parse(chunk)
+                        arr.push(newData)
+                        fs.writeFile("./data/foods.json",JSON.stringify(arr) , err=>{
+                            if(err){
+                                console.log(err);
+                            }else{
+                                console.log("success");
+                            }
+                        })
+
+                    }
+                })
+                
+
+                
+            })
+            request.on("end", ()=>{
+                console.log("end of data");
+            })
+
+        }
+    }
+    response.end("<h1>hello</h1>")
+}).listen(3000)
---
+++ Ozy/Day-03/Modules/writeFileServer.js
@@ -0,0 +1,17 @@
+const fs = require("fs")
+
+
+const change = {
+    name: "test",
+    gender: "female",
+    interest: "nobody"
+}
+
+
+fs.writeFile("./data/test.json", JSON.stringify(change) , err=>{
+    if(err){
+        console.error(err)
+    }else{
+    console.log("success");
+    }
+})
