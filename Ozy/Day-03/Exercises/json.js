d0582a8f46eb54268adbc4da36bc453d8c916868
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-06-24 12:00:48 +0800

    Create modules

---
+++ Ozy/Day-03/Exercises/json.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const JsonFile = `${__dirname}/data/test.json`;
+
+
+function serverJsonFile(request, response){
+    response.setHeader("Content-Type", "application/json")
+      fs.createReadStream(JsonFile)
+      .on("error", () => {
+        console.error("err");
+      })
+      .pipe(response);}
+
+module.exports = serverJsonFile
