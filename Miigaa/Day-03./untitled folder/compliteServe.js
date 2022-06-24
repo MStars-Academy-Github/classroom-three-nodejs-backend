97ff9ae5d9395e27ce59954ae372d16ad53e9189
mstars_lab3_01 <mstars_lab3_01@mstars-lab3-01s-iMac.local>
2022-06-24 12:05:22 +0800

    [add] Day-03

---
+++ Miigaa/Day-03./untitled folder/compliteServe.js
@@ -0,0 +1,29 @@
+var http = require("http");
+const fs = require("fs");
+
+const AudioServerFile = require("./AudioServer")
+const VideoServerFile = require("./VideoServer")
+const ImageServerFile = require("./ImgServer")
+const jsonServerFile = require ("./DataServer")
+
+const JsonFile = `${__dirname}/data/test.json`;
+
+http
+  .createServer((request, response) => {
+    console.log(request.url);
+    if (request.url === "/json") {
+      return jsonServerFile(request, response);
+    } else if (request.url === "/image") {
+      return ImageServerFile(request, response);
+    } else if (request.url === "/audio") {
+      return AudioServerFile(request, response);
+    } else if (request.url === "/video") {
+      return VideoServerFile(request, response);
+    } else {
+      response.end("Not Found");
+    }
+  })
+  .listen(3000);
+
+
+
