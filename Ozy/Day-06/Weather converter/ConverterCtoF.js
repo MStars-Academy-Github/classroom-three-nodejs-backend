780ab49570753c4951c2e5cd279551bbbfe14789
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-06-28 12:07:01 +0800

    Renamed and added new files

--- Ozy/Day-06/Weather converter/ConverterCtoF.js
+++
@@ -1,6 +0,0 @@
-function ConverterCtoF(value) {
-  let fahrenheit = (value * 9) / 5 + 32;
-  console.log(fahrenheit + "Fahrenheit");
-}
-
-module.exports = ConverterCtoF;
--- Ozy/Day-06/Weather converter/ConverterFtoC.js
+++
@@ -1,6 +0,0 @@
-function ConverterFtoC(value) {
-  let celcius = ((value - 32) * 5) / 9;
-  console.log(celcius + "Celcius");
-}
-
-module.exports = ConverterFtoC;
