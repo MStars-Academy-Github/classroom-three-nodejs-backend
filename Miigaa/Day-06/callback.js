6096cd09b15a048bd2551f28ce31c9f45053e57d
mstars_lab3_01 <mstars_lab3_01@mstars-lab3-01s-iMac.local>
2022-06-28 13:20:30 +0800

    [add]Day-06 Miigaa]

---
+++ Miigaa/Day-06/callback.js
@@ -0,0 +1,10 @@
+const printFilms = require("./app");
+
+function printAll() {
+  printFilms("A", () => {
+    printFilms("B", () => {
+      printFilms("C", () => {});
+    });
+  });
+}
+printAll();
---
+++ Miigaa/Day-06/lesson.js
@@ -0,0 +1,8 @@
+const printFilms = require("./app");
+
+function printAll() {
+  printFilms("A");
+  printFilms("B");
+  printFilms("C");
+}
+printAll();
