c880b1c57b0574385a261b5faa6c01aa14461182
Ozy <munkhtamir.g@outlook.com>
2022-06-29 22:58:12 +0800

    Ex02 and Ex03

--- Ozy/Day-07/Assignments/Ex03.js
+++ Ozy/Day-07/Assignments/Ex03.js
@@ -1,1 +1,66 @@
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
+const objectToTable = require("object-to-table");
 
+let convertedData;
+
+https
+  .get("https://ghibliapi.herokuapp.com/films", (res) => {
+    let data = [];
+    res.on("data", (chunk) => {
+      data.push(chunk);
+      fs.writeFile(
+        "./data/films.json",
+        Buffer.concat(data).toString(),
+        (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log("success");
+          }
+        }
+      );
+    });
+    res.on("end", () => {
+      convertedData = Buffer.concat(data).toString();
+    });
+  })
+  .on("error", (err) => {
+    console.error(err);
+  });
+
+http
+  .createServer(function (request, response) {
+    fs.readFile("./data/films.json", "utf-8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      } else {
+        let data1 = JSON.parse(data);
+        response.write(
+          ` <table>
+            <tr>
+              <th scope="col">Numbers</th>
+              <th scope="col">Titles</th>
+              <th scope="col">Images</th>
+            </tr>
+            
+            <tr>
+            <tr>
+            <th >${data1.map((e, i) => i)}</th>
+            
+            
+
+            <th > ${data1.map((e) => e.title)}</th>
+            <th > ${data1.map((e) => e.image)}</th>
+             
+            </tr>
+          </table>`
+        );
+        response.end();
+      }
+    });
+  })
+  .listen(3002);
+console.log("Server running at http://localhost:3002");
