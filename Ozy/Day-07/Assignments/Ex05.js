cd9d21d7f8c68b3b07da3b238a12a444272646c1
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-06-30 13:59:28 +0800

    mod

--- Ozy/Day-07/Assignments/Ex05.js
+++ Ozy/Day-07/Assignments/Ex05.js
@@ -1,5 +1,25 @@
 const http = require("http");
 const fs = require("fs");
+const https = require("https");
+
+function getImage(url) {
+  https
+    .get(url, (res) => {
+      res.on("data", (chunk) => {
+        fs.writeFile("./data/image.json", chunk, (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log("success");
+          }
+        });
+      });
+      res.on("end", () => {});
+    })
+    .on("error", (err) => {
+      console.error(err);
+    });
+}
 
 http
   .createServer(function (request, response) {
@@ -10,21 +30,21 @@ http
           return;
         } else {
           let people = JSON.parse(data);
-          console.log(people[0].films);
+
           response.write(
             ` <table>
                 <tr>
                   <th scope="col">Numbers</th>
                   <th scope="col">Name</th>
-                  <th scope="col">Images</th>
-                  <th scope="col">Eye color</th>
+                  <th scope="col">Gender</th>
+                  <th scope="col">Age</th>
                 </tr>
               
                 ${people.map(
                   (e, i) =>
                     ` <tr><td>${i + 1}</td><td>${e.name}</td><td>${
                       e.gender
-                    }</td><td>${e.eye_color}</td> </tr>`
+                    }</td><td>${e.age}</td> <td>${getImage(e.films)}</td> </tr>`
                 )}
                
               </table>`
@@ -35,3 +55,4 @@ http
     }
   })
   .listen(3000);
+console.log("Running");
