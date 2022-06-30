dd92ab954b77507b2ba1637073b11c5ef2ef8574
mstars_lab3_14 <mstars_lab3_14@mstars-lab3-14s-iMac.local>
2022-06-30 13:16:00 +0800

    push

--- orgil/day-08/chat-server/public/chat.js
+++ orgil/day-08/chat-server/public/chat.js
@@ -2,8 +2,8 @@ new window.EventSource("/sse").onmessage = function (event) {
   window.messages.innerHTML += `<p>${event.data}</p>`;
 };
 
-window.FormData.addEventListener("submit", (evt) => {
+window.form.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  window.fetch(`/chat?message=${window.InputDeviceInfo.value}`);
+  window.fetch(`/chat?message=${window.input.value}`);
   window.input.value = "";
 });
--- orgil/day-08/chat-server/server.js
+++ orgil/day-08/chat-server/server.js
@@ -1,29 +1,45 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
+const eventEmitter = require("events");
+const chatEmitter = new eventEmitter();
 
 http
   .createServer((request, response) => {
-    console.log(request.url);
     if (request.url === "/") {
       response.end("<h1>It is root</h1>");
-    } else if (request.url === "/chat") {
+    } else if (request.url.match(/^\/chat/)) {
       return respondChat(request, response);
     } else if (request.url === "/sse") {
       return respondSSE(request, response);
     } else if (request.url.match(/^\/static/)) {
       return respondStatic(request, response);
     } else {
-      respond.end("Not Found");
+      response.end("Not Found");
     }
   })
   .listen(3000);
 
 function respondChat(req, res) {
-  res.end("chat");
+  const reqParam = url.parse(req.url, true);
+  const chatMessage = reqParam.query.message;
+
+  chatEmitter.emit("message", chatMessage);
+  res.end();
 }
 
 function respondSSE(req, res) {
-  res.end("sse");
+  res.writeHead(200, {
+    "Content-type": "text/event-stream",
+    Connection: "keep-alive",
+  });
+
+  const onMessage = (msg) => res.write(`data: ${msg}\n\n`);
+  chatEmitter.on("message", onMessage);
+
+  res.on("close", function () {
+    chatEmitter.off("message", onMessage);
+  });
 }
 
 // this function serves all files inside public folder in dynamic way
