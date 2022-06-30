15e85d059d0a62e1fd1a815bde2acf15adcf4691
kurtzkaranq <mendbayarerxes@gmail.com>
2022-06-30 13:36:11 +0800

    08 erkhes last in class

--- Erkhes/07/Ex05.js
+++ Erkhes/07/Ex05.js
@@ -1,13 +1,14 @@
 const http = require("http");
 const https = require("https");
 const fs = require("fs");
+let link = [];
 https
-  .get("https://ghibliapi.herokuapp.com/films", (res) => {
+  .get("https://ghibliapi.herokuapp.com/people", (res) => {
     let data = [];
     res.on("data", (chunk) => {
       data.push(chunk);
       fs.writeFile(
-        "./data/films.json",
+        "./data/people.json",
         Buffer.concat(data).toString(),
         (err) => {
           if (err) {
@@ -25,6 +26,44 @@ https
   .on("error", (err) => {
     console.error(err);
   });
+function getLink() {
+  fs.readFile("./data/people.json", "utf-8", (err, data) => {
+    if (err) {
+      console.log("error on reading");
+    } else {
+      const ddata = JSON.parse(data);
+      ddata.map((movie) => {
+        link.push(movie.films[0]);
+      });
+    }
+  });
+  link.map((e) => {
+    https
+      .get(e, (res) => {
+        let ddata = JSON.parse(data);
+        res.on("data", (chunk) => {
+          ddata.push(chunk);
+          fs.writeFile(
+            "./data/link.json",
+            Buffer.concat(data).toString(),
+            (err) => {
+              if (err) {
+                console.error(err);
+              } else {
+                console.log("success");
+              }
+            }
+          );
+        });
+        res.on("end", () => {
+          convertedData = Buffer.concat(data).toString();
+        });
+      })
+      .on("error", (err) => {
+        console.error(err);
+      });
+  });
+}
 https
   .get("https://ghibliapi.herokuapp.com/people", (res) => {
     let data = [];
@@ -50,30 +89,11 @@ https
     console.error(err);
   });
 http
-  .createServer((request, response) => {
+  .createServer(async (request, response) => {
     const tableStart = "<table>";
     const tableEnd = "</table>";
     let endResult = "";
     let result = "";
-    if (request.url === "/ghibli=films") {
-      fs.readFile("./data/films.json", "utf-8", (err, data) => {
-        if (err) {
-          console.log("error");
-        } else {
-          const films = JSON.parse(data);
-          films.map((film, i) => {
-            result += `<tr>
-                        <td>${film.title}</td>
-                        <td>
-                        <img src=${film.image} alt="img" />
-                        </td>
-                        </tr>`;
-          });
-          endResult = tableStart + result + tableEnd;
-          response.write(endResult);
-        }
-      });
-    }
     if (request.url === "/ghibli=people") {
       fs.readFile("./data/people.json", "utf-8", (err, data) => {
         if (err) {
@@ -82,15 +102,16 @@ http
           const people = JSON.parse(data);
           people.map((person, i) => {
             result += `<tr>
-                        <td>${person.name}</td>
-                        <td>
-                        ${person.age}
-                        </td>
-                        <td>
-                        ${person.gender}
-                        </td>
-                        </tr>`;
+                <td>${person.name}</td>
+                <td>
+                ${person.age}
+                </td>
+                <td>
+                ${person.gender}
+                </td>
+                </tr>`;
           });
+
           endResult = tableStart + result + tableEnd;
           response.write(endResult);
         }
