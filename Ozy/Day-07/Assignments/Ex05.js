35440843877f0a91270755a3e4e47c89e84b6f6c
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-06-30 13:04:51 +0800

    Chat

--- Ozy/Day-07/Assignments/Ex05.js
+++ Ozy/Day-07/Assignments/Ex05.js
@@ -1,31 +1,37 @@
-http.createServer(function (request, response) {
-  if (request.url === "/ghibli=films") {
-    fs.readFile("./data/films.json", "utf-8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      } else {
-        let films = JSON.parse(data);
-        response.write(
-          ` <table>
+const http = require("http");
+const fs = require("fs");
+
+http
+  .createServer(function (request, response) {
+    if (request.url === "/ghibli=people") {
+      fs.readFile("./data/people.json", "utf-8", (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        } else {
+          let people = JSON.parse(data);
+          console.log(people[0].films);
+          response.write(
+            ` <table>
                 <tr>
                   <th scope="col">Numbers</th>
-                  <th scope="col">Titles</th>
+                  <th scope="col">Name</th>
                   <th scope="col">Images</th>
+                  <th scope="col">Eye color</th>
                 </tr>
-                
-               
-                ${films.map(
+              
+                ${people.map(
                   (e, i) =>
-                    ` <tr><td>${i + 1}</td><td>${e.title}</td><td><img src=${
-                      e.image
-                    }></td> </tr>`
+                    ` <tr><td>${i + 1}</td><td>${e.name}</td><td>${
+                      e.gender
+                    }</td><td>${e.eye_color}</td> </tr>`
                 )}
                
               </table>`
-        );
-        response.end();
-      }
-    });
-  }
-});
+          );
+          response.end();
+        }
+      });
+    }
+  })
+  .listen(3000);
