45846de01a5ec43b587e8246b02fc9a5c51cbfdf
mstars_lab3_14 <mstars_lab3_14@mstars-lab3-14s-iMac.local>
2022-07-01 13:41:29 +0800

    PUSH

--- orgil/day-07/ex-5.js
+++ orgil/day-07/ex-5.js
@@ -5,22 +5,46 @@ const http = require("http");
 const getPeople = util.promisify(https.get);
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
+let answer;
 
 http
-  .createServer((req, res) => {
-    getPeople("https://ghibliapi.herokuapp.com/people", (result) => {
-      console.log(result.statusCode);
-      let data = [];
-      result.on("data", (chunk) => {
-        data.push(chunk);
-      });
-    })
-      .then((test) => {
-        console.log(test);
+  .createServer(async (req, res) => {
+    const people = await getPeople(
+      "https://ghibliapi.herokuapp.com/people",
+      (result) => {
+        let data = [];
+        result.on("data", (chunk) => {
+          data.push(chunk);
+        });
+        result.on("end", async () => {
+          const convertedData = JSON.parse(Buffer.concat(data).toString());
+          //   console.log(convertedData);
+          answer = convertedData;
+          await writeFile(
+            "./data/ex-5-data.json",
+            JSON.stringify(convertedData),
+            (err) => {
+              if (err) {
+                console.log(err);
+              }
+            }
+          );
+        });
+        res.end("Ending..1");
+        return console.log("sda");
+      }
+    )
+      .then((data) => {
+        console.log(data);
+        console.log("this is data");
+        res.end("Ending..2");
       })
       .catch((err) => {
+        console.log("this is error");
         console.log(err);
+        res.end("Ending..3");
       });
-    res.end("HELLO");
+    // console.log(people);
+    res.end("Ending..4");
   })
   .listen(3004);
