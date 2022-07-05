9df55f665149d5f70eabe74ff50825caee2f190c
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-07-05 11:19:53 +0800

    ex03

---
+++ Ozy/Day-10/ex03/ex03.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const EventEmitter = require("events");
+const fs = require("fs");
+const eventEmitter = new EventEmitter();
+
+const onMessage = () => {
+  fs.readFile("../data/films.json", "utf-8", (err, data) => {
+    const films = JSON.parse(data);
+    if (err) {
+      console.error(err);
+    } else {
+      `<table><tr><th>${films.map(
+        (e, i) =>
+          ` <tr><td>${i + 1}</td><td>${e.title}</td><td><img src=${
+            e.image
+          }></td> </tr>`
+      )}}</th></tr></table>`;
+    }
+  });
+};
+eventEmitter.on("message", onMessage);
+
+http
+  .createServer((request, response) => {
+    if (request.url === "/films/show") {
+      eventEmitter.emit("message");
+    }
+    response.end();
+  })
+  .listen(3002);
+console.log("Running");
