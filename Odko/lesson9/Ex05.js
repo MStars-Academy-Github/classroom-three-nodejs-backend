268b9cb66bdf7bb2897955faa8975e9354765fd3
mstars_lab3_12 <mstars_lab3_12@mstars-lab3-12s-iMac.local>
2022-07-05 15:01:01 +0800

    Add lesson 10

---
+++ Odko/lesson9/Ex05.js
@@ -0,0 +1,121 @@
+const http = require("http");
+const util = require("util");
+const https = require("https");
+const httpsGet = util.promisify(https.get);
+const EventEmitter = require("events");
+const fs = require("fs");
+const readFile = util.promisify(fs.readFile);
+const serverEvents = new EventEmitter();
+let people;
+let films;
+
+http
+  .createServer((req, res) => {
+    res.setHeader(200, "Content-Type", "text/html");
+    if (req.url === "/ghibli=people") {
+      peopleServer();
+      serverEvents.emit("people");
+    } else if (req.url === "/ghibli=films") {
+      filmsServer();
+      serverEvents.emit("films");
+    }
+
+    // fs.readFile(`${__dirname}/data/people.html`)
+    //   .then((contents) => {
+    //     res.setHeader("Content-Type", "text/html");
+    //     res.writeHead(200);
+    //     res.end(contents);
+    //   })
+    //   .catch((err) => {
+    //     res.writeHead(500);
+    //     res.end(err);
+    //     return;
+    //   });
+  })
+  .listen(3001);
+console.log("running 3000");
+
+// runnning html
+
+function peopleServer() {
+  httpsGet("https://ghibliapi.herokuapp.com/people", (res) => {
+    let data = [];
+    res.on("data", (chunk) => {
+      data.push(chunk);
+    });
+    res.on("end", () => {
+      const DATA = JSON.parse(Buffer.concat(data).toString());
+      serverEvents.on("people", () => {
+        fs.writeFile("./data/people.json", JSON.stringify(DATA), (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("success");
+          }
+        });
+        readFile("./data/people.json", "utf-8")
+          .then((data) => {
+            people = JSON.parse(data);
+            let table = `<table>
+            ${people.map((a, i) => {
+              return `  <tr>
+                  <td>${a.name}</td>
+                  <td>${a.age}</td>
+                  <td>${a.gender}</td>
+                </tr>`;
+            })}
+          </table>`;
+            fs.writeFile("./data/people.html", table, (err) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("success");
+              }
+            });
+          })
+          .catch((err) => console.log(err));
+      });
+    });
+  });
+}
+
+function filmsServer() {
+  httpsGet("https://ghibliapi.herokuapp.com/films", (res) => {
+    let data = [];
+    res.on("data", (chunk) => {
+      data.push(chunk);
+    });
+    res.on("end", () => {
+      const DATA = JSON.parse(Buffer.concat(data).toString());
+      serverEvents.on("films", () => {
+        fs.writeFile("./data/films.json", JSON.stringify(DATA), (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("success");
+          }
+        });
+        readFile("./data/films.json", "utf-8")
+          .then((data) => {
+            films = JSON.parse(data);
+            let table = `<table>
+              ${films.map((a, i) => {
+                return `  <tr>
+                    <td>${a.title}</td>
+                    <td><img src=${a.image} alt="" style="width: 150px;"></td>
+                  </tr>`;
+              })}
+            </table>`;
+            fs.writeFile("./data/films.html", table, (err) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("success");
+              }
+            });
+          })
+          .catch((err) => console.log(err));
+      });
+    });
+  });
+}
