79ae2c8f84e6f18295cdf771ca1d23046e1ec803
Ozy <munkhtamir.g@outlook.com>
2022-07-06 00:33:18 +0800

    Ex03 and 04

--- Ozy/Day-10-Express/ex03/ex03.js
+++ Ozy/Day-10-Express/ex03/ex03.js
@@ -3,29 +3,39 @@ const EventEmitter = require("events");
 const fs = require("fs");
 const eventEmitter = new EventEmitter();
 
-const onMessage = () => {
+eventEmitter.on("films", () => {
   fs.readFile("../data/films.json", "utf-8", (err, data) => {
     const films = JSON.parse(data);
-    console.log(films);
     if (err) {
       console.error(err);
     } else {
-      `<table><tr><th>${films.map(
-        (e, i) =>
-          ` <tr><td>${i + 1}</td><td>${e.title}</td><td><img src=${
-            e.image
-          }></td> </tr>`
-      )}}</th></tr></table>`;
+      const tableOfFilms = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+        </head>
+        <body>
+        <table><tr><th>${films.map(
+          (e, i) =>
+            `<tr><td>${i + 1}</td><td>${e.title}</td><td><img src=${
+              e.image
+            }></td> </tr>`
+        )}}</th></tr></table></body>
+      </html>`;
+      fs.writeFile("./public/films.html", tableOfFilms, (err) => {
+        console.error(err);
+      });
     }
   });
-};
-eventEmitter.on("message", onMessage);
-eventEmitter.off("message", onMessage);
-
+});
 http
   .createServer((request, response) => {
     if (request.url === "/films/show") {
-      eventEmitter.emit("message");
+      eventEmitter.emit("films");
+      console.log("Success");
     }
     response.end();
   })
