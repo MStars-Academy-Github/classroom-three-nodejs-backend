8477f24054bf5bf4bff140925b4f49b22f4beb5b
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-07-20 13:03:22 +0800

    MOD

--- Ozy/Day-13-Validator/index.js
+++ Ozy/Day-13-Validator/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { body, validationResult, check } = require("express-validator");
+const validatorMiddleware = require("./validatorMiddleware.js");
+const { userValidationRules, validate } = require("./validatorMiddleware");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
@@ -9,31 +10,9 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.post(
-  "/users",
-  body("email").isEmail(),
-  body("register").isLength({ min: 10, max: 10 }),
-  body("phone")
-    .isNumeric()
-    .isLength({ min: 8, max: 8 })
-    .withMessage("only phone number"),
-  check("password")
-    .isLength({ min: 8 })
-    .matches(/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/)
-    .withMessage(
-      "Checks that a password has a minimum of 6 characters, at least 1 uppercase letter, 1 lowercase letter, and 1 number with no spaces."
-    ),
-  check("name")
-    .matches(/^[a-zA-Z]+$/)
-    .withMessage("Name must only string"),
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    res.send("Success");
-  }
-);
+app.post("/users", userValidationRules(), validate, (req, res) => {
+  res.send("Success");
+});
 
 app.get(
   "/user/:id",
