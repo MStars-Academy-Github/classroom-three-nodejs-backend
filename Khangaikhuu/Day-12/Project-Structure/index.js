9115867fad46924958a7916c9cc4a3ebc928b12b
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2022-07-20 12:38:29 +0800

    [ADD] day 12

--- Khangaikhuu/Day-12/Project-Structure/index.js
+++ Khangaikhuu/Day-12/Project-Structure/index.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.static("public"));
 
+app.get("/api/foods", (req, res) => {
+  res.send("it is serving foods");
+});
+
 app.get("/api", (req, res) => {
   res.send("it is working");
 });
