27b181f04513e81e1573ed82560a8ef3023fb7b1
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2022-07-20 14:43:21 +0800

    [ADD] validator middle ware

--- Khangaikhuu/Day-13-Validation/index.js
+++ Khangaikhuu/Day-13-Validation/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { body, validationResult } = require("express-validator");
+const { userValidationRules, validate } = require("./validatorMiddleware.js");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
@@ -9,26 +9,9 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
-app.post(
-  "/users",
-  body("email").isEmail(),
-  body("register").isLength({ min: 10, max: 10 }),
-  (req, res) => {
-    const body = req.body;
-    console.log(body);
-    const { name, email, phone } = body;
-    console.log(name);
-    console.log(email);
-    console.log(phone);
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    res.send("users");
-  }
-);
+app.post("/users", userValidationRules(), validate, (req, res) => {
+  res.send("users");
+});
 
 app.listen(PORT, () => {
   "Express server is starting";
