8ca57135da9243a0b4ea98964278b4171a0a2163
sumiya <sumiya.udval@gmail.com>
2022-07-21 16:00:42 +0800

    add api-s

--- sumiya/Day-14 assignment/index.js
+++ sumiya/Day-14 assignment/index.js
@@ -2,41 +2,128 @@ const express = require("express");
 const validator = require("express-validator");
 const fs = require("fs");
 const moment = require("moment");
-
 const router = express.Router();
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
-
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
 app.set("views", __dirname + "/views");
 app.set("view options", { layout: false });
 app.set("view engine", "ejs");
 app.use(router);
-router.get("/", (req, res, next) => {
-  res.send("Hey Hey!");
+
+readFile("./public/book.json", "utf-8", (err, booksData) => {
+  if (err) {
+    console.error(err);
+  } else {
+    books = JSON.parse(booksData);
+  }
 });
 
 function getMultipleRandom(arr, num) {
   const shuffled = [...arr].sort(() => 0.5 - Math.random());
-
   return shuffled.slice(0, num);
 }
 
+router.get("/", (req, res, next) => {
+  res.send("Hey Hey!");
+});
+
+// 1. Хэрэглэгч номын систем рүү нэвтрэх бүрт санамсаргүй байдлаар 3 номыг хардаг байна
 router.get("/books", (req, res, next) => {
-  const data = fs.readFileSync("./public/book.json", "utf-8");
-  const jsonData = JSON.parse(data);
-  const filterData = getMultipleRandom(jsonData.books, 3);
-  console.log(typeof filterData);
-  //   const dateSortData = data.books.map((book) => {
-  //     return moment().format(JSON.parse(book.published), "YYYY");
-  //   });
-  //   console.log(dateSortData);
-  //   const filterData = data.books.sort((date1, date2) => {
-  //     return date1.published - date2.published;
-  //   });
-  //   console.log(filterData);
-  //   console.log(typeof data);
+  const filterData = getMultipleRandom(books.books, 3);
+  //   console.log(typeof filterData);
   res.render("index", { data: filterData });
+});
+
+// 2. Хасгийн сүүлээс эхэн хүртэл хэвлэгдсэн дарааллаар номын мэдээллийг авна.
+router.get("/books/sort", (req, res, next) => {
+  let sortby = books.books;
+  let sorted = sortby.sort((a, b) => {
+    return (
+      moment(JSON.stringify(b.published), "YYYY") -
+      moment(JSON.stringify(a.published), "YYYY")
+    );
+  });
+  //   console.log(typeof sorted);
+  res.send(sorted);
+});
+
+//3. манай номын сан дахь бүх зохиолчдын нэрийг авмаар байна.
+router.get("/books/authors", (req, res) => {
+  const authors = [];
+  books.books.map((a) => {
+    return authors.push(a.author);
+  });
+  res.send(authors);
+});
+
+//5. ISBN дугаараар ноPIн Pэдээлэл буцаах (localhost/book/isbn_id).
+router.get("/books/isbn/:id", (req, res) => {
+  const isbn = JSON.parse(req.params.id);
+  const newBook = [];
+  books.books.find((books) => {
+    if (books.isbn == isbn) {
+      newBook.push(books);
+    }
+  });
+  console.log(newBook);
+  res.send(newBook);
+});
+
+//4. Бүх номын мэдээллийг авах api.
+router.get("/all", (req, res, next) => {
+  res.render("allbook", { data: books.books });
+});
+
+//6. номын нэрээр хайлт хийх api (localhost/search?title=”js”)
+router.get("/books/search/:title", (req, res) => {
+  const searchTitle = JSON.stringify(req.params.title);
+  console.log(searchTitle);
+  const newBook = [];
+  books.books.filter((books) => {
+    if (
+      JSON.stringify(books.title)
+        .toLocaleLowerCase()
+        .trim()
+        .match(searchTitle.toLocaleLowerCase().trim())
+    ) {
+      newBook.push(books);
+    }
+  });
+  console.log(newBook);
+  res.send(newBook);
+});
+
+function maxValue(...args) {
+  const max = args.reduce((acc, val) => {
+    return acc > val ? acc : val;
+  });
+  return max;
+}
+//7. Хамгийн их хуудастай номын мэдээлэл авах.
+router.get("/maximiumPageNumber", (req, res) => {
+  const arr = books.books.map((book, i) => {
+    return book[i];
+  });
+  //   console.log(JSON.stringify(arr));
+  const max = maxValue(...arr);
+  res.send(max);
+});
+function minValue(...args) {
+  const min = args.reduce((acc, val) => {
+    return acc < val ? acc : val;
+  });
+  return min;
+}
+
+//7. Хамгийн бага хуудастай номын мэдээлэл авах.
+router.get("/minimiumPageNumber", (req, res) => {
+  const arr = books.books.pages;
+  const minimium = minValue(...arr);
+  //   console.log(newBook);
+  res.send(minimium);
 });
 
 router.get("/add", (req, res, next) => {
