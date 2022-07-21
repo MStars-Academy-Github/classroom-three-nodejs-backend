541c289e9b2109c0e87e8e74d5b25df16a3e752b
mstars_lab3_14 <mstars_lab3_14@mstars-lab3-14s-iMac.local>
2022-07-21 12:55:49 +0800

    push

--- orgil/book/index.js
+++ orgil/book/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
+const books = require("./public/books.json");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -11,7 +12,12 @@ app.set("views", __dirname + "/views");
 app.set("view options", { layout: false });
 
 router.get("/", (req, res, next) => {
-  res.send("Hello nigga");
+  let temp = books;
+  const shuffled = books.sort(() => 0.5 - Math.random());
+  let selected = shuffled.slice(0, 3);
+  console.log(selected);
+
+  res.render("index", { data: books });
 });
 router.get("/add", (req, res, next) => {
   res.render("addBook");
