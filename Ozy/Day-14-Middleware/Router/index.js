555fa48dc9e4c7e14c18586bf981cecd2c08e55d
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-07-21 10:24:02 +0800

    Day-14

--- Ozy/Day-14-Middleware/Router/index.js
+++ Ozy/Day-14-Middleware/Router/index.js
@@ -5,10 +5,36 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 
 router.get("/userinfo/:id", (req, res, next) => {
-  res.send("User Info with ID");
+  const id = req.params.id;
+  if (id < 0) {
+    const err = new Error("Can't find user with this ID!");
+    err.status = "fail";
+    err.statusCode = 500;
+    return next(err);
+  }
+  res.send("User Info with ID " + id);
 });
-router.use("/user", (req, res, next) => {
-  res.send("User Info");
+
+router.get(
+  "/user1/:id",
+  (req, res, next) => {
+    const id = req.params.id;
+    if (id < 10) {
+      const err = new Error("Cant find user with this ID!");
+      err.status = "fail";
+      next(err);
+    }
+    res.send("User Info with ID " + id);
+  },
+  (err, req, res, next) => {
+    res.status(500).json({ message: err.status });
+  }
+);
+router.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
 });
 
 app.use("/", router);
