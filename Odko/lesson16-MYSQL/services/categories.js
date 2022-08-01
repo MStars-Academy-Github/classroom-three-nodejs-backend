69cac835002607acbff83a84f883f6ed4babf749
mstars_lab3_12 <mstars_lab3_12@mstars-lab3-12s-iMac.local>
2022-08-01 13:21:32 +0800

    Add file

---
+++ Odko/lesson16-MYSQL/services/categories.js
@@ -0,0 +1,28 @@
+const db = require("../db");
+
+async function getAllCategories() {
+  const data = await db.query("SELECT id, name, color, v FROM category");
+  const params = {};
+
+  return {
+    data,
+    params,
+  };
+}
+
+async function createCategory(params) {
+  const name = params.name;
+  const color = params.color;
+  const v = params.v;
+  const data = await db.query(
+    "INSERT INTO category ( name, color, v) VALUES (?, ?, ?)",
+    [name, color, v]
+  );
+  return {
+    data,
+  };
+}
+module.exports = {
+  getAllCategories,
+  createCategory,
+};
