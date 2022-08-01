2477f30b28f59dd50ca70683248f7b8dfa634369
mstart_lab3_19 <mstart_lab3_19@mstart-lab3-19s-iMac.local>
2022-08-01 12:20:52 +0800

    [ADD] mysql day 5 connecting nodejs with mysql

---
+++ Khangaikhuu/MySQL-Day-05/services/categories.js
@@ -0,0 +1,15 @@
+const db = require("../db");
+
+async function getAllCategories() {
+  const data = await db.query("SELECT id, name, color FROM category");
+  const params = {};
+
+  return {
+    data,
+    params,
+  };
+}
+
+module.exports = {
+  getAllCategories,
+};
