b2a12d8cc1b040589ac1a1475796da634b809f00
kurtzkaranq <mendbayarerxes@gmail.com>
2022-08-01 11:07:16 +0800

    8.1

---
+++ Erkhes/mysql-day-05/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+
+
+
+app.get('/' , (req,res)=>{
+	res.send('My app is running')
+})
+
+app.listen(PORT)
