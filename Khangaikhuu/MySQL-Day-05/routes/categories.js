15943dcefd544d61640aeae114cb8c4b2f65a570
mstart_lab3_19 <mstart_lab3_19@mstart-lab3-19s-iMac.local>
2022-08-01 12:49:58 +0800

    [ADD] create category with post request

--- Khangaikhuu/MySQL-Day-05/routes/categories.js
+++ Khangaikhuu/MySQL-Day-05/routes/categories.js
@@ -11,4 +11,14 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.post("/", async (req, res, next) => {
+  try {
+    const params = req.body;
+    res.json(await categories.createCategory(params));
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
+});
+
 module.exports = router;
