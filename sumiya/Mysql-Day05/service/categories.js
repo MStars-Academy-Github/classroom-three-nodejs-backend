886414b3fb3269272159e1d1a74302a898141128
sumiya <sumiya.udval@gmail.com>
2022-08-01 13:39:06 +0800

    mysql add crud

---
+++ sumiya/Mysql-Day05/service/categories.js
@@ -0,0 +1,50 @@
+const db = require("../db");
+
+async function getAllCategories() {
+  const data = await db.query("select * from categories");
+  const params = {};
+  return {
+    data,
+    params,
+  };
+}
+async function createCategory(params) {
+  const name = params.name;
+  const color = params.color;
+  const _v = params._v;
+  const data = await db.query(
+    "insert into categories (name, color, _v) values(?, ?, ?)",
+    [name, color, _v]
+  );
+
+  return {
+    data,
+  };
+}
+async function deleteCategory(params) {
+  const id = params._id;
+  const data = await db.query("delete from categories where _id =?", [id]);
+  return {
+    data,
+  };
+}
+async function updateCategory(params) {
+  const id = params._id;
+  const name = params.name;
+  const color = params.color;
+  const _v = params._v;
+  const data = await db.query(
+    "update categories set name=?, color =? , _v=? where _id=?",
+    [name, color, _v, id]
+  );
+  return {
+    data,
+  };
+}
+
+module.exports = {
+  getAllCategories,
+  createCategory,
+  deleteCategory,
+  updateCategory,
+};
