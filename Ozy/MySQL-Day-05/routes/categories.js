f7903259aae5b20f4573d9b8eccf05e827eaacdd
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-08-01 13:22:20 +0800

    MySQL CRUD

--- Ozy/MySQL-Day-05/routes/categories.js
+++ Ozy/MySQL-Day-05/routes/categories.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const categories = require("../services/categories");
 
-router.get("./", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     res.json(await categories.getAllCategories());
   } catch (err) {
@@ -11,4 +11,33 @@ router.get("./", async (req, res, next) => {
   }
 });
 
+router.post("/", async (req, res, next) => {
+  try {
+    const params = req.body;
+    res.json(await categories.createCategory(params));
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
+});
+
+router.delete("/", async (req, res, next) => {
+  try {
+    const params = req.body;
+    res.json(await categories.deleteCategory(params));
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
+});
+
+router.put("/", async (req, res, next) => {
+  try {
+    const params = req.body;
+    res.json(await categories.updateCategory(params));
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
+});
 module.exports = router;
