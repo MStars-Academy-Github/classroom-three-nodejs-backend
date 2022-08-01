b2b45774ee24cf80bb669cf41f5942f683fd9efe
mstars_lab3_01 <mstars_lab3_01@mstars-lab3-01s-iMac.local>
2022-08-01 13:32:16 +0800

    [add]

--- Miigaa/MySql-Day-15/index.js
+++ Miigaa/MySql-Day-15/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const router = express.Router();
 const app = express();
 require("dotenv").config();
+const categoryRouter = require("./routes/categories");
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use("/api/categories", categoryRouter);
 
 app.get("/", (req, res) => {
   res.send("My app is running");
