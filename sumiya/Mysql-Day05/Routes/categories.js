e3afb8b3d7ea1fa298608e933ca46c070ff8279b
sumiya <sumiya.udval@gmail.com>
2022-08-02 16:03:56 +0800

    add

--- sumiya/Mysql-Day05/Routes/categories.js
+++ sumiya/Mysql-Day05/Routes/categories.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET, PUT, POST, DELETE,PATCH",
+  })
+);
 const router = express.Router();
-
+app.use("/category", router);
 const categories = require("../service/categories");
 
-router.get("/get", async (req, res, next) => {
+router.get("/get", cors(), async (req, res, next) => {
   try {
     res.json(await categories.getAllCategories());
   } catch (err) {
@@ -12,7 +21,7 @@ router.get("/get", async (req, res, next) => {
   }
 });
 
-router.post("/insert", async (req, res, next) => {
+router.post("/insert", cors(), async (req, res, next) => {
   try {
     const params = req.body;
     res.json(await categories.createCategory(params));
@@ -24,13 +33,14 @@ router.post("/insert", async (req, res, next) => {
 router.delete("/delete", async (req, res, next) => {
   try {
     const params = req.body;
+    console.log(params);
     res.json(await categories.deleteCategory(params));
   } catch (error) {
     console.error(error.message);
     next(error);
   }
 });
-router.put("/update", async (req, res, next) => {
+router.put("/update", cors(), async (req, res, next) => {
   try {
     const params = req.body;
     res.json(await categories.updateCategory(params));
