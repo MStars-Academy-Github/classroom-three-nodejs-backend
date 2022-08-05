4c923384bcbc7bd7916ed977cf0e00f336d09314
mstars_lab3_12 <mstars_lab3_12@mstars-lab3-12s-iMac.local>
2022-08-05 16:35:48 +0800

    Add file

--- Odko/lesson16-MYSQL/services/foods.js
+++ Odko/lesson16-MYSQL/services/foods.js
@@ -14,7 +14,7 @@ async function createFood(params) {
   const discount = params.discount;
   const sales = params.sales;
   const category_id = params.category_id;
-  const name = params.name;
+  const foodName = params.foodName;
   const price = params.price;
   const portion = params.portion;
   const stock = params.stock;
@@ -22,12 +22,12 @@ async function createFood(params) {
   const tumb_img = params.tumb_img;
   const ingredients = params.ingredients;
   const data = await db.query(
-    "INSERT INTO foods ( discount, sales, category_id, name,price,portion,stock,image,tumb_img,ingredients) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    "INSERT INTO foods ( discount, sales, category_id, foodName,price,portion,stock,image,tumb_img,ingredients) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
     [
       discount,
       sales,
       category_id,
-      name,
+      foodName,
       price,
       portion,
       stock,
@@ -47,8 +47,70 @@ async function deleteFood(params) {
     data,
   };
 }
+
+async function getFoodById(id) {
+  const data = await db.query("SELECT * FROM foods WHERE _id=?", [id]);
+  const params = {};
+  return {
+    data,
+    params,
+  };
+}
+async function getFoodJoinFood(id) {
+  const data = await db.query(
+    `SELECT *  FROM foods f LEFT JOIN category c ON f.category_id = c.id `
+    // `SELECT f.discount foodDiscount, f.sales foodSales,
+    // f.category_id foodCategory_id, f.name foodName,
+    // f.price foodPrice, f.portion foodPortion, f.stock foodStock,
+    // f.image foodImage, f.tumb_img foodTumb_img, f.ingredients foodIngredients,
+    // c.id categoryID, c.name categoryName, c.color categoryColor, c.v categoryV
+    // FROM foods f LEFT JOIN category c ON f.category_id = c.id `
+  );
+  const params = {};
+  return {
+    data,
+    params,
+  };
+}
+
+async function updateFood(params) {
+  const _id = params._id;
+  const discount = params.discount;
+  const sales = params.sales;
+  const category_id = params.category_id;
+  const foodName = params.foodName;
+  const price = params.price;
+  const portion = params.portion;
+  const stock = params.stock;
+  const image = params.image;
+  const tumb_img = params.tumb_img;
+  const ingredients = params.ingredients;
+
+  const data = await db.query(
+    "UPDATE foods SET discount =?, sales=?, category_id=?, foodName=?, price=?, portion=?, stock=?, image=?, tumb_img=?, ingredients=? WHERE _id=? ",
+    [
+      discount,
+      sales,
+      category_id,
+      foodName,
+      price,
+      portion,
+      stock,
+      image,
+      tumb_img,
+      ingredients,
+      _id,
+    ]
+  );
+  return {
+    data,
+  };
+}
 module.exports = {
   getAllFood,
   deleteFood,
   createFood,
+  getFoodById,
+  getFoodJoinFood,
+  updateFood,
 };
