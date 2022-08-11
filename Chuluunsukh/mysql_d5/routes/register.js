6897a9b98796e7205f895f7254e996eba9621334
mstars_lab_04 <mstars_lab_04@mstars-lab-04s-iMac.local>
2022-08-11 10:48:05 +0800

    user added

--- Chuluunsukh/mysql_d5/routes/register.js
+++ Chuluunsukh/mysql_d5/routes/register.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const { registerUser } = require("../service/register");
 const router = express.Router();
+const userService = require("../service/userService");
 
 router.post("/register", async (req, res, next) => {
   try {
     const params = req.body;
     console.log(params);
+    // when the request body is empty object
     if (Object.values(params).length === 0) {
       res.statusCode(400).json({
         success: false,
         message: "No user data is provided",
       });
     }
+    // check the email address already exist in DB
+    const { email } = params;
+    const existUser = await userService.findUserByEmail(email);
+    if (existUser.data.length > 0) {
+      res.status(400).json({
+        success: false,
+        message: "user already exists",
+      });
+    }
+    console.log(existUser);
+    // call register service
     const user = await register.registerUser(params);
     res.json("It's register side");
   } catch {
