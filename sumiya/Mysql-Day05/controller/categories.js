72c933d2351116e9fe13a79460c10f998b7ece2f
sumiya <sumiya.udval@gmail.com>
2022-08-11 13:39:24 +0800

    add authorization

--- sumiya/Mysql-Day05/controller/categories.js
+++ sumiya/Mysql-Day05/controller/categories.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
+const auth = require("../middleware/auth");
 const router = express.Router();
 const categories = require("../service/categories");
 
-router.get("/", async (req, res, next) => {
+router.get("/", auth, async (req, res, next) => {
   try {
     res.json(await categories.getAllCategories());
   } catch (err) {
@@ -12,7 +12,7 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.post("/insert", async (req, res, next) => {
+router.post("/", auth, async (req, res, next) => {
   try {
     const params = req.body;
     console.log(params);
@@ -22,7 +22,7 @@ router.post("/insert", async (req, res, next) => {
     next(error);
   }
 });
-router.delete("/delete", async (req, res, next) => {
+router.delete("/", async (req, res, next) => {
   try {
     const params = req.body;
     console.log(params);
