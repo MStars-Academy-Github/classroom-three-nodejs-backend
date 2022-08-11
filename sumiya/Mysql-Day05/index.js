3866b7d5e88de2b2424604f44b7e53ec54b75ec4
sumiya <sumiya.udval@gmail.com>
2022-08-11 13:00:31 +0800

    add register login authentication

--- sumiya/Mysql-Day05/index.js
+++ sumiya/Mysql-Day05/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
-const categoriesRouter = require("./Routes/categories");
-const foodrouter = require("./Routes/food");
-const user = require("./Routes/user");
-const role = require("./Routes/role");
+const categoriesRouter = require("./controller/categories");
+const foodrouter = require("./controller/food");
+const user = require("./controller/user");
+const role = require("./controller/role");
+const api = require("./controller/AunthenticationController");
 require("dotenv").config();
 
 const PORT = process.env.PORT;
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use("/category", categoriesRouter);
 app.use("/food", foodrouter);
 app.use("/user", user);
+app.use("/api", api);
 app.use("/role", role);
 
 // app.get("/", (req, res) => {
