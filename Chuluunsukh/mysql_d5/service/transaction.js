cf96f5725fa7e8e455fedb4a72429f3f984e67f3
mstars_lab_04 <mstars_lab_04@mstars-lab-04s-iMac.local>
2022-08-17 12:18:35 +0800

    feat: transaction folder added

--- Chuluunsukh/mysql_d5/service/transaction.js
+++ Chuluunsukh/mysql_d5/service/transaction.js
@@ -1,29 +1,36 @@
 const db = require("../db");
 
 async function createOrder() {
-  db.query(`SET AUTOCOMMIT = 0`);
-  console.log("setting off the autocommit");
+  const connection = await mysql.createConnection(config.db);
+  await connection.execute(`SET AUTOCOMMIT = 0`);
 
-  db.query(`SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ`);
-  console.log("setting the transaction isolation level");
-  db.beginTransaction();
-  console.log("starting transaction");
+  connection.execute(`SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITED`);
+  console.log("setting transaction isolation level");
 
+  await connection.beginTransaction();
+  console.log("starting connection");
   try {
-    db.query(
+    connection.execute(
       `INSERT INTO orders(customer_id, deliveryman_id, ordered_date, order_status, total_fee) values (?, ?, ?, ?, ?)`,
-      "2022-08-15 12:33",
-      1,
-      6000
+      [1, 5, "2022-08-15 12:33", 1, 6000]
     );
 
-    db.query(`SET @id = LAST_INSERT_ID()`);
-    console.log("Getting last id of the new inserted order");
-    db.query(
-      `INSERT INTO order_detail (food_id, food_price, order_id) values (?, ?, ?), [1,1800, "@id"]`
+    console.log("inserted data into orders");
+
+    const [rows] = await connection.execute(
+      "SELECT LAST_INSERT_ID() as order_id"
     );
-    db.commit();
-    console.log("commited successfully");
+    console.log(rows);
+    console.log("Getting last id of the new inserted order");
+
+    connection.execute(
+      `INSERT INTO order_detail (food_id, food_price, order_id) values (?, ?, ?)`,
+      [1, 1800, rows[0].order_id]
+    );
+    console.log("insert data into order detail");
+
+    connection.commit();
+    console.log("commit successfully");
   } catch (error) {
     consoler.error("Error occured while creating order:" + error.message);
     db.rollBack();
