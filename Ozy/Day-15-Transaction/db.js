012cfc3c7ec3e68a7b10ceba60036937c17d6edc
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-08-17 12:37:38 +0800

    Add transaction

---
+++ Ozy/Day-15-Transaction/db.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2/promise");
+const config = require("./config");
+const pool = mysql.createPool(config.db);
+const connection = mysql.createConnection(config.db);
+
+async function query(sql, params) {
+  const [rows, fields] = await pool.execute(sql, params);
+  return rows;
+}
+
+async function beginTransaction() {
+  return await connection.beginTransaction();
+}
+
+async function rollBack() {
+  return await connection.rollBack();
+}
+
+async function commit() {
+  return await connection.commit();
+}
+
+module.exports = { query, beginTransaction, rollBack, commit };
