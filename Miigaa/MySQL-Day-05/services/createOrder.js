439bbec5fbe4f65eb41547d98d1af915d477ca8c
mstars_lab3_01 <mstars_lab3_01@mstars-lab3-01s-iMac.local>
2022-08-18 12:19:21 +0800

    [add]

--- Miigaa/MySQL-Day-05/services/createOrder.js
+++ Miigaa/MySQL-Day-05/services/createOrder.js
@@ -1,31 +1,43 @@
 const db = require("../db");
+const mysql = require("mysql2/promise");
+const config = require("../config");
 
 async function createOrder() {
-  db.query(`SET AUTOCOMMIT = 0`);
+  const connection = await mysql.createConnection(config.db);
+  await connection.execute(`SET AUTOCOMMIT = 0`);
   console.log("setting off the autocommit");
-  db.query(`Set SESSION TRANSACTION ISOLATION LEVEL READ COMMIT`);
+
+  connection.execute(`Set SESSION TRANSACTION ISOLATION LEVEL READ COMMIT`);
   console.log("setting the transaction isolation level");
-  db.beginTransaction;
+
+  await connection.beginTransaction();
   console.log("starting transaction");
 
   try {
-    db.query(
-      "INSERT INTO orders (customer_id, deliveryman_id, ordered_date, order_status, total_fee) values (?,?,?,?,?)",
+    connection.execute(
+      "INSERT INTO orders (customer_id, deliveryman_id, ordered_date, order_status, total_fee) values ( ?, ?, ?, ?, ?)",
       [1, 5, "2022-8-15 12:34:00", 1, 6000]
     );
-    db.query("Set @id = LAST_INSERT_ID()");
-    db.query(
-      "INSERT INTO order_detail (food_id, food_price, order_id) values (?,?,?)",
-      [1, 1800, "@id"]
+    console.log("inserted data into orders");
+
+    const [rows] = connection.execute("Select LAST_INSERT_ID() as order_id");
+    console.log("Getting last id of the new inserted order");
+
+    connection.execute(
+      "INSERT INTO order_detail (food_id, food_price, order_id) values ( ?, ?, ?)",
+      [1, 1800, rows[0].order_id]
     );
     console.log("Getting last id of the new inserted order");
-    db.commit();
+
+    connection.commit();
     console.log("commit successfully");
   } catch (error) {
     console.error("Error occurred while creating order: " + error.message);
-    db.rollBack();
+    connection.rollBack();
     console.error("transaction is successfully rolled back");
   }
 }
 
-module.exports = { createOrder };
+(async function testCreateOrder() {
+  console.log(await createOrder());
+})();
