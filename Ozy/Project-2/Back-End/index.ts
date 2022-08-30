9ab248045d491b64bb62c161e7fdfeb5c8f06b7a
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-08-30 12:41:51 +0800

    Init Project 2

--- Ozy/Project-2/Back-End/index.ts
+++ Ozy/Project-2/Back-End/index.ts
@@ -1,10 +1,14 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
 const app: Express = express();
 dotenv.config();
 const PORT = process.env.PORT;
 app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Project 2");
+});
 
 app.listen(PORT, () => {
   console.log("Server is running at " + PORT);
