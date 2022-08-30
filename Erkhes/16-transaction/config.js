37728f13e144e5d805a0a3e7f635fe8aa1502763
kurtzkaranq <mendbayarerxes@gmail.com>
2022-08-30 12:42:07 +0800

    test

--- Erkhes/16-transaction/config.js
+++ Erkhes/16-transaction/config.js
@@ -5,11 +5,6 @@ const MYSQL_USER = process.env.USER_NAME;
 const MYSQL_PASSWORD = process.env.PASSWORD;
 const MYSQL_DATABASE = process.env.DATABASE;
 
-console.log(HOST);
-console.log(MYSQL_USER);
-console.log(MYSQL_PASSWORD);
-console.log(MYSQL_DATABASE);
-
 const config = {
   db: {
     host: HOST,
--- Erkhes/16-transaction/index.js
+++ Erkhes/16-transaction/index.js
@@ -2,46 +2,51 @@ const mysql = require("mysql2/promise");
 const config = require("./config");
 
 async function createOrder() {
-  const items = ["RI002", "CB004"];
+  const items = ["RI0002", "CB0004"];
 
   const connection = await mysql.createConnection(config.db);
-  console.log("server successfully connected");
+  console.log("Mysql Server succesfully connected");
 
   await connection.execute("SET TRANSACTION ISOLATION LEVEL READ COMMITTED");
 
   await connection.beginTransaction();
   try {
     await connection.execute(
-      "SELECT id , name from product where sku in (?, ?) for UPDATE",
+      "SELECT id, name FROM product WHERE sku IN (?, ?) FOR UPDATE",
       items
     );
+
     const [itemsToOrder] = await connection.execute(
-      "SELECT name , quantity , price from product where sku in (?, ?) Order by id",
+      "SELECT name, quantity, price FROM product WHERE sku in (?, ?) ORDER BY id",
       items
     );
+
     let orderTotal = 0;
     let orderItems = [];
 
     for (const itemToOrder of itemsToOrder) {
       if (itemToOrder.quantity < 1) {
         throw new Error(
-          "one of the items is out of stock" + itemToOrder.quantity
+          `One of the itesm is out of stock ${itemToOrder.quantity}`
         );
-      } else {
-        orderTotal += itemToOrder.price;
-        orderItems.push(itemToOrder.name);
       }
+
+      orderTotal += itemToOrder.price;
+      orderItems.push(itemToOrder.name);
     }
+
     await connection.execute(
-      "insert into sales_order (items, total) values(?, ?)",
+      "INSERT INTO sales_order (items, total) VALUES (?, ?)",
       [orderItems.join(), orderTotal]
     );
+
     console.log("Order created");
 
     await connection.execute(
-      `Update product set quantity = quantity-1 where sku in (?, ?)`,
+      `UPDATE product SET quantity=quantity-1 WHERE sku IN (?,?)`,
       items
     );
+
     await connection.commit();
     const [rows] = await connection.execute(
       "SELECT LAST_INSERT_ID() as order_id"
@@ -49,9 +54,9 @@ async function createOrder() {
 
     return `order created with id ${rows[0].order_id}`;
   } catch (error) {
-    console.error(error.message);
+    console.error(error);
     connection.rollback();
-    console.info("Rollback successful");
+    console.info("Rollback succesful");
     return "error creating order";
   }
 }
@@ -60,3 +65,4 @@ async function createOrder() {
   console.log(await createOrder());
   process.exit(0);
 })();
+ 
