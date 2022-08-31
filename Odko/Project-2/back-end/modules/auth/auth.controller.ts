3d79ead5cc48afe368338f01dbd8fa56ac186d90
mstars_lab3_12 <mstars_lab3_12@mstars-lab3-12s-iMac.local>
2022-08-31 12:26:06 +0800

    AAd file

---
+++ Odko/Project-2/back-end/modules/auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import * as authService from "./auth.service";
+import { tokenService } from "../token";
+
+export const loginUser = async (req: Request, res: Response) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  const user = await authService.loginUser(email, password);
+  const tokens = await tokenService.generateAuthToken(user);
+  console.log(tokens);
+  res.send({ user, tokens });
+};
