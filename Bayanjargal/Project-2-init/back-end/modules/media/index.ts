01c078919e0b49a0e066827d8f2d289701843837
mstars_lab3_15 <mstars_lab3_15@mstars-lab3-15s-iMac.local>
2022-09-02 11:21:27 +0800

    feat:video upload and save to mondodb

---
+++ Bayanjargal/Project-2-init/back-end/modules/media/index.ts
@@ -0,0 +1,5 @@
+import Media from "./media.model";
+import * as mediaInterface from "./media.interface";
+import * as mediaController from "./media.controller";
+
+export { mediaController, Media, mediaInterface };
--- Bayanjargal/Project-2-init/back-end/routes/v1/index.ts
+++ Bayanjargal/Project-2-init/back-end/routes/v1/index.ts
@@ -2,6 +2,7 @@ import express, { Router } from "express";
 
 import userRoute from "./user.route";
 import authRoute from "./auth.route";
+import medaiRoute from "./media.route";
 const router = express.Router();
 
 interface IRoute {
@@ -11,5 +12,6 @@ interface IRoute {
 
 router.use("/users", userRoute);
 router.use("/auth", authRoute);
+router.use("/media", medaiRoute);
 
 export default router;
