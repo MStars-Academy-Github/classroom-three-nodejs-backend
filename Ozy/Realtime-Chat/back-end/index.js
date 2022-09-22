c6bd401833d1215d7f4df0e61b93246b5fbf1b0e
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-09-22 11:07:52 +0800

    MOD

--- Ozy/Realtime-Chat/back-end/index.js
+++ Ozy/Realtime-Chat/back-end/index.js
@@ -4,6 +4,8 @@ const app = express();
 const PORT = process.env.PORT;
 const mongoose = require("mongoose");
 const MONGO_SERVER = process.env.ATLAS_MONGO_SERVER;
+const socketIo = require("socket.io");
+const cors = require("cors");
 
 const http = require("http");
 const server = http.createServer(app);
@@ -12,9 +14,39 @@ app.get("/", (req, res) => {
   res.json("root path");
 });
 
-mongoose.connect(MONGO_SERVER).then(() => {
-  console.log("Conneted MongoDB");
-  server.listen(PORT, () => {
-    console.log("Server running at " + PORT);
+io = socketIo(server);
+io.on("connection", (socket) => {
+  socket.on("join", ({ userName }, callback, error) => {
+    if (error) {
+      callback(error);
+    } else {
+      const roomId = 123;
+      socket.join(roomId);
+      console.log(userName);
+    }
+
+    socket.on("inputChange", ({ value }, callback, error) => {
+      if (error) {
+        callback(error);
+      } else {
+        console.log(value);
+      }
+    });
+    socket.on("disconnect", () => {
+      console.log("disconnected");
+    });
   });
 });
+
+mongoose
+  .connect(MONGO_SERVER)
+  .then(() => {
+    console.log("Conneted MongoDB");
+    server.listen(PORT, () => {
+      console.log("Server running at " + PORT);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
